8bcb9be0253d0e00d7a53c1cf72997ffd0b85138
sskim91 <tjdtjq91@gmail.com>
2021-03-16 16:03:37 +0900

    Feat: Hello Servlet

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sskim on 2021/03/16
+ * Github : http://github.com/sskim91
+ */
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("<h1>hello " + username + "</h1>");
+    }
+}
