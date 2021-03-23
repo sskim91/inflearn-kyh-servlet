0a594ce5395f0debef9d0aa5040574d9d6cf9444
sskim91 <tjdtjq91@gmail.com>
2021-03-23 23:16:52 +0900

    Feat: 스프링 MVC - 시작하기

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * Created by sskim on 2021/03/23
+ * Github : http://github.com/sskim91
+ */
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
+}
